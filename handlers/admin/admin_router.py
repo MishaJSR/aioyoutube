bf628081d710099fa5fc6e580006d3c49e14ab05
develophub <mixan_45@mail.ru>
2024-12-28 15:57:29 +0300

    1.0.0 fix grpc all

--- handlers/admin/admin_router.py
+++ handlers/admin/admin_router.py
@@ -1,8 +1,8 @@
+import asyncio
 from os.path import split
 
 from aiogram import types, Router, F
 from aiogram.filters import CommandStart
-import validators
 
 from base_settings import base_settings
 
@@ -11,26 +11,40 @@ user_bot_id = base_settings.get_user_bot_id()
 static_reg = "regxstate"
 static_status = "progress"
 
-def is_valid_url(url):
-    return validators.url(url)
-
 @admin_main_router.message(CommandStart())
 async def user_start(message: types.Message):
     await message.answer("Чтобы бот вам отправил видео с ютуб, отправьте ему ссылку")
 
-@admin_main_router.message()
+#
+# @admin_main_router.message(F.text == "Привет")
+# async def user_start(message: types.Message):
+#     mess = await message.answer("Чтобы бот вам отправил видео с ютуб, отправьте ему ссылку")
+#     await asyncio.sleep(0.1)
+#     await message.bot.edit_message_text(
+#         text=f"sdasdasdad",
+#         chat_id=message.chat.id,
+#         message_id=mess.message_id,
+#     )
+
+@admin_main_router.message(F.video)
 async def user_start(message: types.Message):
-    if message.chat.id != user_bot_id:
-        if is_valid_url(message.text):
-            await message.bot.send_message(chat_id=user_bot_id, text=message.text+"`"+str(message.chat.id))
-            return
-        else:
-            await message.answer("URL неопределен")
-            return
-    else:
+    print("get_video")
+    try:
         if message.video:
             mes, user_id = message.caption.split(static_reg)
             await message.bot.send_video(chat_id=user_id, video=message.video.file_id)
-        else:
-            mes, user_id = message.text.split(static_reg)
-            await message.bot.send_message(chat_id=user_id, text=mes)
+    except:
+        await message.answer(text="Ошибка, отправьте URL")
+
+@admin_main_router.message()
+async def user_start(message: types.Message):
+    if message.chat.id != user_bot_id:
+        await message.bot.send_message(chat_id=user_bot_id, text=message.text+"`"+str(message.chat.id))
+#     else:
+#         if message.video:
+#             mes, user_id = message.caption.split(static_reg)
+#             await message.bot.send_video(chat_id=user_id, video=message.video.file_id)
+#         else:
+#             mes, user_id = message.text.split(static_reg)
+#             await message.bot.send_message(chat_id=user_id, text=mes)
+
--- serve.py
+++ serve.py
@@ -1,19 +1,61 @@
 import asyncio
 
 import grpc
-from concurrent import futures
+import validators
+from aiogram import Bot
 
-from grpc_utils import message_pb2, message_pb2_grpc
+from grpc_utils.proto import message_pb2, message_pb2_grpc
+
+async def start_progress_bar(bot, tg_user_id, duration_in_seconds, mess_id):
+    duration = duration_in_seconds / 200
+    num_values = 10
+    interval = duration / num_values
+    for i in range(num_values):
+        await asyncio.sleep(interval)
+        progress = round((i + 1) * (100 / num_values))
+        await bot.edit_message_text(
+            text=f"Статус отправки: {progress}%",
+            chat_id=tg_user_id,
+            message_id=mess_id,
+        )
+
+
 
 class MessagingServiceServicer(message_pb2_grpc.MessageServiceServicer):
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, bot: Bot):
+        self.bot: Bot = bot
+        self.last_message_id = None
+
+    def is_valid_url(self, url):
+        return validators.url(url)
 
     async def SendMessage(self, request, context):
-        print(f"Received progress: {request.text}")
-        print(f"For chat: {request.tg_user_id}")
+        text = request.text
+        tg_user_id = request.tg_user_id
+        type_mess = request.type_mess
+        print(text, tg_user_id, type_mess)
         response = message_pb2.Message(text="Message received: " + request.text)
-        await self.bot.send_message(chat_id=request.tg_user_id, text=request.text)
+        if type_mess == "url":
+            if self.is_valid_url(text):
+                mess = await self.bot.send_message(chat_id=tg_user_id, text="Начало загрузки")
+                self.last_message_id = mess.message_id
+            else:
+                await self.bot.send_message(chat_id=tg_user_id, text="URL неопределен")
+        if type_mess == "error_load":
+            await self.bot.send_message(chat_id=tg_user_id, text="Ошибка загрузки")
+        if type_mess == "error_server":
+            await self.bot.send_message(chat_id=tg_user_id, text="Ошибка на стороне сервиса")
+        if type_mess == "send_video":
+            m = await self.bot.send_message(chat_id=tg_user_id, text="Отправляю видео")
+            asyncio.create_task(start_progress_bar(bot=self.bot, tg_user_id=tg_user_id, duration_in_seconds=int(text),
+                                                   mess_id = m.message_id))
+            self.last_message_id = None
+        if type_mess == "progress":
+            await self.bot.edit_message_text(
+                text=f"Статус загрузки: {text}%",
+                chat_id=request.tg_user_id,
+                message_id=self.last_message_id,
+            )
         return response
 
 
