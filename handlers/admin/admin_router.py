a0e9e61d98e02ddd9a7c45812da47709c75bb406
develophub <mixan_45@mail.ru>
2025-01-13 10:44:57 +0300

    1.0.0 fix some router

--- handlers/admin/admin_router.py
+++ handlers/admin/admin_router.py
@@ -15,17 +15,6 @@ static_status = "progress"
 async def user_start(message: types.Message):
     await message.answer("Чтобы бот вам отправил видео с ютуб, отправьте ему ссылку")
 
-#
-# @admin_main_router.message(F.text == "Привет")
-# async def user_start(message: types.Message):
-#     mess = await message.answer("Чтобы бот вам отправил видео с ютуб, отправьте ему ссылку")
-#     await asyncio.sleep(0.1)
-#     await message.bot.edit_message_text(
-#         text=f"sdasdasdad",
-#         chat_id=message.chat.id,
-#         message_id=mess.message_id,
-#     )
-
 @admin_main_router.message(F.video)
 async def user_start(message: types.Message):
     print("get_video")
@@ -40,11 +29,5 @@ async def user_start(message: types.Message):
 async def user_start(message: types.Message):
     if message.chat.id != user_bot_id:
         await message.bot.send_message(chat_id=user_bot_id, text=message.text+"`"+str(message.chat.id))
-#     else:
-#         if message.video:
-#             mes, user_id = message.caption.split(static_reg)
-#             await message.bot.send_video(chat_id=user_id, video=message.video.file_id)
-#         else:
-#             mes, user_id = message.text.split(static_reg)
-#             await message.bot.send_message(chat_id=user_id, text=mes)
+        print("send")
 
--- serve.py
+++ serve.py
@@ -6,25 +6,28 @@ from aiogram import Bot
 
 from grpc_utils.proto import message_pb2, message_pb2_grpc
 
-async def start_progress_bar(bot, tg_user_id, duration_in_seconds, mess_id):
-    duration = duration_in_seconds / 150
-    num_values = 10
-    interval = duration / num_values
-    for i in range(num_values):
-        await asyncio.sleep(interval)
-        progress = round((i + 1) * (100 / num_values))
-        await bot.edit_message_text(
-            text=f"Статус отправки: {progress}%",
-            chat_id=tg_user_id,
-            message_id=mess_id,
-        )
 
+async def start_progress_bar(bot, tg_user_id, duration_in_seconds, mess_id, stop_event):
+    while not stop_event.is_set():
+        addition = "."
+        for i in range(3):
+            await bot.edit_message_text(
+                text=f"Видео отправляется {addition}",
+                chat_id=tg_user_id,
+                message_id=mess_id,
+            )
+            addition += "."
+            await asyncio.sleep(1)
+    await bot.delete_message(chat_id=tg_user_id, message_id=mess_id)
 
 
 class MessagingServiceServicer(message_pb2_grpc.MessageServiceServicer):
     def __init__(self, bot: Bot):
         self.bot: Bot = bot
         self.last_message_id = None
+        self.message_status_id = None
+        self.message_send_video_id = None
+        self.stop_event = asyncio.Event()
 
     def is_valid_url(self, url):
         return validators.url(url)
@@ -33,31 +36,39 @@ class MessagingServiceServicer(message_pb2_grpc.MessageServiceServicer):
         text = request.text
         tg_user_id = request.tg_user_id
         type_mess = request.type_mess
-        print(text, tg_user_id, type_mess)
         response = message_pb2.Message(text="Message received: " + request.text)
+        if type_mess == "position":
+            await self.bot.send_message(chat_id=tg_user_id, text=f"{text}")
         if type_mess == "url":
-            if self.is_valid_url(text):
-                mess = await self.bot.send_message(chat_id=tg_user_id, text="Начало загрузки")
-                self.last_message_id = mess.message_id
-            else:
-                await self.bot.send_message(chat_id=tg_user_id, text="URL неопределен")
-        if type_mess == "error_load":
-            await self.bot.send_message(chat_id=tg_user_id, text="Ошибка загрузки")
-        if type_mess == "error_server":
-            await self.bot.send_message(chat_id=tg_user_id, text="Ошибка на стороне сервиса")
-        if type_mess == "send_video":
-            m = await self.bot.send_message(chat_id=tg_user_id, text="Отправляю видео")
-            asyncio.create_task(start_progress_bar(bot=self.bot, tg_user_id=tg_user_id, duration_in_seconds=int(text),
-                                                   mess_id = m.message_id))
-            self.last_message_id = None
+            url, img_url, description = text.split("`")
+            await self.bot.send_photo(chat_id=tg_user_id, photo=f"{img_url}", caption=f"{description}")
+            mess = await self.bot.send_message(chat_id=tg_user_id, text="Начало загрузки")
+            self.last_message_id = mess.message_id
         if type_mess == "progress":
-            await self.bot.edit_message_text(
-                text=f"Статус загрузки: {text}%",
-                chat_id=request.tg_user_id,
-                message_id=self.last_message_id,
-            )
+            mess = await self.bot.edit_message_text(text=f"Статус загрузки: {text}%",
+                                             chat_id=request.tg_user_id,
+                                             message_id=self.last_message_id)
+            self.message_status_id = mess.message_id
+        if type_mess == "error_load":
+            await self.bot.send_message(chat_id=tg_user_id, text=f"{text}")
+        if type_mess == "error_server":
+            await self.bot.send_message(chat_id=tg_user_id, text=f"{text}")
+        if type_mess == "send_video":
+            await self.bot.delete_message(chat_id=tg_user_id, message_id=self.message_status_id)
+            self.message_status_id = None
+            m = await self.bot.send_message(chat_id=tg_user_id, text="Отправляю видео")
+            self.message_send_video_id = m.message_id
+            asyncio.create_task(start_progress_bar(bot=self.bot, tg_user_id=tg_user_id, duration_in_seconds=int(text),
+                                                   mess_id=self.message_send_video_id, stop_event=self.stop_event))
+            self.last_message_id = None
+        if type_mess == "video_delivered":
+            await stop_task(self.stop_event)
+            self.stop_event = asyncio.Event()
         return response
 
+
+async def stop_task(stop_event: asyncio.Event):
+    stop_event.set()
 
 
 async def serve(bot):
